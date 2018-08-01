17babd2f21e7d52126df9e3eeeb29868e6434660
Unknown <mcguirk.eric@gmail.com>
2018-07-31 23:19:54 -0400

    Finished newStrip.py

--- Chapter 7/newStrip.py
+++ Chapter 7/newStrip.py
@@ -1,7 +1,14 @@
 import re
-import pyperclip
+import pyperclip as pc
 
 
-def newStrip(text, char = '\s'):
-	removeChar = r'^(' + re.escape(char) + r')*([^' + re.escape(char) + '])+(' + re.escape(char) + r')*$'
-	
+def newStrip(text, char = ' '):
+	'''
+	text: str; text surrounded by another character
+	char: default is space; character that needs to be removed from text
+	Copies output to the clipboard
+	'''
+	assert type(text) == str
+	regex = r'^(' + re.escape(char) + r')*(.*?)(' + re.escape(char) + r')*$'  #Constructs a raw string to be used as a regex
+	removeChar = re.compile(regex).search(text).groups()[1]
+	pc.copy(removeChar)
